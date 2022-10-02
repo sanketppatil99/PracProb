a6ffcc55062a233dd0410ab5d4db8982f919860d
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-10-02 10:50:48 +0530

    Program for Power of 2

---
+++ PowerofTwo/PowerofTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace PowerofTwo
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the value of 'N' below to find table of power 2 ..");
+            Console.WriteLine("Enter the value N between 0 and 31 =  ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n >= 0 && n < 31)
+            {
+                for (int i = 0; i <= n; i++)
+                {
+                    double pow = Math.Pow(2, i);
+                    Console.WriteLine("2 ^ {0} is = {1}", i, pow);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Entered number not valid");
+            }
+        }
+    }
+}
