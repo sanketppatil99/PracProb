954b1157dd2dcc67c3a17038c36fdca3622380b1
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-10-02 10:03:50 +0530

    Program for leapYear

---
+++ leapYear/leapYear/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace leapYear
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 400;
+            int b = 100;
+            int c = 4;
+            Console.WriteLine("Enter the year in Four digit =  ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            if ((userInput % a) == 0)
+            {
+                Console.WriteLine(" {0} is Leap Year ", userInput);
+            }
+            else if ((userInput % b) == 0)
+            {
+                Console.WriteLine(" {0} is not a leap year ", userInput);
+            }
+            else if ((userInput % c) == 0)
+            {
+                Console.WriteLine("{0} is leap Year ", userInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not a leap Year", userInput);
+            }
+        }
+    }
+}
